b4e6f1ce87c2e029bb148088c2b48998361e4d65
Bill Duncan <wdduncan@gmail.com>
2020-07-02 14:53:00 -0400

    add conrolled term values for mixs triad values and quantity values for depth

--- metadata-translation/src/bin/align_nmdc_datatypes.py
+++ metadata-translation/src/bin/align_nmdc_datatypes.py
@@ -8,7 +8,7 @@ from collections import namedtuple
 from pprint import pprint
 import pandas as pds
 import jsonasobj
-import nmdc
+from nmdc import Biosample, GeolocationValue, ControlledTermValue, QuantityValue, Activity, OntologyClass
 import lib.data_operations as dop
 
 # TODO: convert triad value in complex object; convert depth the measurement unit ("meters")
@@ -30,20 +30,35 @@ def convert_env_triad(attribute_value):
     curie_val = attribute_value_to_string(attribute_value)
     curie_val = curie_val.replace('_', ':')
     
-    return nmdc.ControlledTermValue(curie_val)
+    obj = ControlledTermValue(has_raw_value=curie_val, term=OntologyClass(id=curie_val))
+    return json.loads(jsonasobj.as_json(obj))
 
-def attribute_value_to_datatype(attribute_value, data_type='string'):
+
+def attribute_value_to_QuantityValue(attribute_value, unit):
+    val = f"{attribute_value['has_raw_value']} {unit}"
+    obj = QuantityValue(val)
+    return json.loads(jsonasobj.as_json(obj))
+
+
+def attribute_value_to_datatype(attribute_value, data_type='string', unit=""):
     val = attribute_value['has_raw_value']
     if data_type == 'integer':
         return int(val)
+    elif data_type == 'float':
+        return float(val)
+    elif data_type == 'quantity value':
+        return attribute_value_to_QuantityValue(attribute_value, unit)
     else:
         return str(val)
+
 
 def attribute_value_to_int(attribute_value):
     return attribute_value_to_datatype(attribute_value, 'integer')
 
+
 def attribute_value_to_string(attribute_value):
     return attribute_value_to_datatype(attribute_value, 'string')
+
 
 def align_data_object(file_path):
     json_list = get_json(file_path) # load json
@@ -53,12 +68,15 @@ def align_data_object(file_path):
         item['file_size_bytes'] = attribute_value_to_int(item['file_size_bytes'])
 
     save_json(json_list, file_path) # save json
-    
+
+
 def align_emsl_data_object():
     align_data_object('output/nmdc_etl/emsl_data_objects.json')
 
+
 def align_jgi_data_object():
     align_data_object('output/nmdc_etl/faa_fna_fastq_data_objects.json')
+
 
 def align_biosample(file_path):
     json_list = get_json(file_path) # load json
@@ -66,9 +84,12 @@ def align_biosample(file_path):
     ## change file size bytes to int
     for item in json_list:
         item['env_broad_scale'] = convert_env_triad(item['env_broad_scale'])
-        print(item['env_broad_scale'])
+        item['env_local_scale'] = convert_env_triad(item['env_local_scale'])
+        item['env_medium'] = convert_env_triad(item['env_medium'])
+        if 'depth' in item.keys(): item['depth'] = attribute_value_to_datatype(item['depth'], 'quantity value', 'm')
 
     save_json(json_list, file_path) # save json
+
 
 def align_gold_biosample():
     align_biosample('output/nmdc_etl/gold_biosample.json')
