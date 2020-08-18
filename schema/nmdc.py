091bf67c5e92c11eedb2a8bfb6349bb21b3db8f5
deepakunni3 <deepak.unni3@gmail.com>
2020-08-18 23:15:32 +0000

    Regenerate artifacts from nmdc.yaml

--- schema/nmdc.py
+++ schema/nmdc.py
@@ -1,5 +1,5 @@
 # Auto generated from nmdc.yaml by pythongen.py version: 0.4.0
-# Generation date: 2020-08-11 14:57
+# Generation date: 2020-08-18 23:15
 # Schema: NMDC
 #
 # id: https://microbiomedata/schema
