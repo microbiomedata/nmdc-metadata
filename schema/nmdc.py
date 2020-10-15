4e69003788b9969d3f0f11e2c92863dd466de9e3
Bill Duncan <wdduncan@gmail.com>
2020-10-15 19:02:05 -0400

    re-generate schema using make

--- schema/nmdc.py
+++ schema/nmdc.py
@@ -1,5 +1,5 @@
 # Auto generated from nmdc.yaml by pythongen.py version: 0.4.0
-# Generation date: 2020-10-06 19:42
+# Generation date: 2020-10-15 17:23
 # Schema: NMDC
 #
 # id: https://microbiomedata/schema
