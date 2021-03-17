d237a190d290b15de505a806babd5f3a1eef64f4
Bill Duncan <wdduncan@gmail.com>
2021-03-17 19:44:43 -0400

    regenerated py file

--- schema/nmdc.py
+++ schema/nmdc.py
@@ -1,5 +1,5 @@
 # Auto generated from nmdc.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-03-16 20:04
+# Generation date: 2021-03-17 19:42
 # Schema: NMDC
 #
 # id: https://microbiomedata/schema
