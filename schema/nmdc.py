7badbc3413b6659f17599856b693352278725494
Bill Duncan <wdduncan@gmail.com>
2021-03-31 11:48:03 -0400

    add fixed schema

--- schema/nmdc.py
+++ schema/nmdc.py
@@ -1,5 +1,5 @@
 # Auto generated from nmdc.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-03-17 19:42
+# Generation date: 2021-03-31 11:46
 # Schema: NMDC
 #
 # id: https://microbiomedata/schema
